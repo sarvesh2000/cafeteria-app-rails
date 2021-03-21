38f4df997ddf54e9f7e065c2da5c733863d57b05
Sarvesh Sankaranarayanan <sarvesh@Sarveshs-MacBook-Pro.local>
2021-03-21 12:29:10 +0530

    Create New Items Done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,26 @@
 class ItemsController < ApplicationController
     def index
-        
+        @items = Item.all
     end
 
+    def create
+        @items = Item.new(items_params)
+        if @items.save
+            #flash[:notice] = "Article was created successfully."
+            @items.save()
+            redirect_to items_path(@item)
+            # or we can write like this
+            #redirect_to @article
+        else
+            render "new"
+        end
+    end
+
+    def new
+        @items = Item.new
+    end
+
+    def items_params
+        params.permit(:item_name, :available_stock)
+    end
 end
