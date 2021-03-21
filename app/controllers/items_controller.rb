cbf6b61548bcc215ba429f46ccec0b9dbb6de94d
Sarvesh Sankaranarayanan <sarvesh@Sarveshs-MacBook-Pro.local>
2021-03-21 15:03:47 +0530

    Updating Items Done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,15 @@
 class ItemsController < ApplicationController
+    before_action :set_item, only: [:show, :update, :edit, :destroy]
+    
     def index
         @items = Item.all
     end
 
     def create
-        @items = Item.new(items_params)
-        if @items.save
+        @item = Item.new(items_params)
+        if @item.save
             #flash[:notice] = "Article was created successfully."
-            @items.save()
+            @item.save()
             redirect_to items_path(@item)
             # or we can write like this
             #redirect_to @article
@@ -17,10 +19,26 @@ class ItemsController < ApplicationController
     end
 
     def new
-        @items = Item.new
+        @item = Item.new
+    end
+
+    def edit
+    end
+
+    def update
+        if @item.update(items_params)
+            @item.save()
+            redirect_to(items_path)
+        else
+            render "edit"
+        end
     end
 
     def items_params
-        params.permit(:item_name, :available_stock)
+        params.require(:item).permit(:item_name, :available_stock)
+    end
+
+    def set_item
+        @item = Item.find(params[:id])
     end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,4 @@
 class Item < ApplicationRecord
+    validates :item_name, presence: true, length: { minimum: 3 }
+    validates :available_stock, presence: true
 end
